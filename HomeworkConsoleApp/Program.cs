58888835683833d847805f39cc33ae60497074ef
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-03-21 15:14:24 -0500

    Level 1 Complete

--- HomeworkConsoleApp/Program.cs
+++ HomeworkConsoleApp/Program.cs
@@ -10,7 +10,23 @@ namespace HomeworkConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string helloWorld = ("Hello World!");
+            int columbusYear = 1492;
+            bool dog = true;
+            DateTime theDate = new DateTime (2017, 3 , 21);
+            double cheeseBurger = 5.45;
+            char theSpot = ('X');
+
+
+            Console.WriteLine(helloWorld);
+            Console.WriteLine("Columbus Sailed the world in " + columbusYear);
+            Console.WriteLine("It is " + dog + " that I have a dog at home.");
+            Console.WriteLine("Today is " + theDate);
+            Console.WriteLine("I spent $" + cheeseBurger + " on a cheeseburger");
+            Console.WriteLine(theSpot + " marks the spot");
+
+
+
         }
     }
 }
